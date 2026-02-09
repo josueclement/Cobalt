4dd51c2142935780c99a68934bff172b8de4b4de
Josué Clément <josue.d.clement@gmail.com>
2026-02-09 13:10:14 +0100

    Add SettingsCardControl

---
+++ Cobalt.Avalonia.Desktop/Controls/SettingsCardControl.cs
@@ -0,0 +1,66 @@
+using System.Windows.Input;
+using Avalonia;
+using Avalonia.Controls.Primitives;
+using Avalonia.Input;
+using Avalonia.Media;
+
+namespace Cobalt.Avalonia.Desktop.Controls;
+
+public class SettingsCardControl : TemplatedControl
+{
+    public static readonly StyledProperty<string?> HeaderProperty =
+        AvaloniaProperty.Register<SettingsCardControl, string?>(nameof(Header));
+
+    public static readonly StyledProperty<string?> DescriptionProperty =
+        AvaloniaProperty.Register<SettingsCardControl, string?>(nameof(Description));
+
+    public static readonly StyledProperty<Geometry?> IconDataProperty =
+        AvaloniaProperty.Register<SettingsCardControl, Geometry?>(nameof(IconData));
+
+    public static readonly StyledProperty<ICommand?> CommandProperty =
+        AvaloniaProperty.Register<SettingsCardControl, ICommand?>(nameof(Command));
+
+    public static readonly StyledProperty<object?> CommandParameterProperty =
+        AvaloniaProperty.Register<SettingsCardControl, object?>(nameof(CommandParameter));
+
+    public string? Header
+    {
+        get => GetValue(HeaderProperty);
+        set => SetValue(HeaderProperty, value);
+    }
+
+    public string? Description
+    {
+        get => GetValue(DescriptionProperty);
+        set => SetValue(DescriptionProperty, value);
+    }
+
+    public Geometry? IconData
+    {
+        get => GetValue(IconDataProperty);
+        set => SetValue(IconDataProperty, value);
+    }
+
+    public ICommand? Command
+    {
+        get => GetValue(CommandProperty);
+        set => SetValue(CommandProperty, value);
+    }
+
+    public object? CommandParameter
+    {
+        get => GetValue(CommandParameterProperty);
+        set => SetValue(CommandParameterProperty, value);
+    }
+
+    protected override void OnPointerPressed(PointerPressedEventArgs e)
+    {
+        base.OnPointerPressed(e);
+
+        if (Command is { } command && command.CanExecute(CommandParameter))
+        {
+            command.Execute(CommandParameter);
+            e.Handled = true;
+        }
+    }
+}
---
+++ Enigma.Avalonia/Views/SettingsPageView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Enigma.Avalonia.Views;
+
+public partial class SettingsPageView : UserControl
+{
+    public SettingsPageView()
+    {
+        InitializeComponent();
+    }
+}
