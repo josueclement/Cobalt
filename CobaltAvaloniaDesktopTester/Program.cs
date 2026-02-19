52985562d73b84ce38fe59c948a7abfe43fd682f
Josué Clément <josue.d.clement@gmail.com>
2026-02-19 20:37:54 +0100

    Fix exception at app shutdown on linux

--- CobaltAvaloniaDesktopTester/Program.cs
+++ CobaltAvaloniaDesktopTester/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Avalonia;
 using System;
+using System.Threading.Tasks;
 
 namespace CobaltAvaloniaDesktopTester;
 
@@ -9,8 +10,19 @@ sealed class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        // On Linux, the DBus connection disposal races with the Avalonia
+        // dispatcher shutdown, throwing a TaskCanceledException on the
+        // thread pool. Swallow it so the process exits cleanly.
+        AppDomain.CurrentDomain.UnhandledException += (_, e) =>
+        {
+            if (e.ExceptionObject is TaskCanceledException)
+                Environment.Exit(0);
+        };
+
+        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
