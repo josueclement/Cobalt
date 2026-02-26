99877c749346c5d7c4e443f46536031688b7ecef
Josué Clément <josue.d.clement@gmail.com>
2026-02-26 21:55:20 +0100

    Rename solution

--- Cobalt.Iam/Models/Role.cs
+++
@@ -1,8 +0,0 @@
-namespace Cobalt.Iam.Models
-{
-    public class Role
-    {
-        public string Id { get; set; } = string.Empty;
-        public string Name { get; set; } = string.Empty;
-    }
-}
--- Cobalt.Localization/Models/Translation.cs
+++
@@ -1,9 +0,0 @@
-namespace Cobalt.Localization.Models
-{
-    public class Translation
-    {
-        public string LanguageCode { get; set; } = string.Empty;
-        public string Key { get; set; } = string.Empty;
-        public string Text { get; set; } = string.Empty;
-    }
-}
